106764e608e08f1da834d4f09b70dfd0db9a0c2a
Indranil Naskar <82460475+indra0421@users.noreply.github.com>
2023-04-13 09:36:12 +0530

    database connected

--- database/db.js
+++ database/db.js
@@ -1,7 +1,16 @@
 import mongoose from 'mongoose';
 
-const Connection = async (username, password) => {                                                    
-    const URL = `mongodb://localhost:27017/nsecblog`
+// const Connection = async (username, password) => {                                                    
+//     const URL = `mongodb://localhost:27017/nsecblog`
+//     try {
+//         await mongoose.connect(URL)
+//         console.log('Database connected successfully');
+//     } catch (error) {
+//         console.log('Error while connecting to the database ', error);
+//     }
+// };
+const Connection = async (username, password) => {
+    const URL = `mongodb+srv://indra0412:fufUcNpW0iY78Fa4@cluster0.8twhafo.mongodb.net/?retryWrites=true&w=majority`
     try {
         await mongoose.connect(URL)
         console.log('Database connected successfully');
